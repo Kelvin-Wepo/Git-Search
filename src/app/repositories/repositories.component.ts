6f577ea77a4f1a0a601f9aa1793d994556e5c7e5
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-18 04:01:09 +0300

    ft;this commit modifies repositories component in typescript

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from '../api.service';
 
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
-  styleUrls: ['./repositories.component.css']
+  styleUrls: ['./repositories.component.css'],
+  providers:[APIService]
 })
 export class RepositoriesComponent implements OnInit {
-
-  constructor() { }
-
+  userName:any;
+  public info:any=[];
+  public repos:any=[];
+  public profiles:any=[]
+  constructor(private apiService:APIService) { 
+  this.apiService.getUserInfo().subscribe(info => {
+    this.info = info;
+  });
+  this.apiService.getUserRepos().subscribe(repos => {
+    this.repos=repos;
+  });
+}
   ngOnInit(): void {
+    this.apiService.getProfiles().subscribe((data)=> {
+      this.profiles = data;
+    })
   }
 
 }
