6105c40dd47fad0254075375c8bad3f89effced9
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-18 04:23:26 +0300

    ft;this component modifies navigation component in ts

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from '../api.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  providers:[APIService]
 })
 export class NavigationComponent implements OnInit {
-
+userName:any;
+public info:any=[];
+public repos:any=[];
+public profiles:any=[]
   constructor() { }
 
   ngOnInit(): void {
