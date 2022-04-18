a8827dc563c1dcc9ba2dabdb1d8f9cc74d89bfc3
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-04-18 03:52:50 +0300

    feat;this commit modifies Services in typescript

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,69 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
+private userName:string;
+private user:any;
+private _Url = 'https://api.github.com/users';
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+    console.log('service is ready')
+    this.userName='Kelvin-wepo';
+  
+  }ngOnInit() {
+    // GET request with response type <any>
+    this.http.get<any>('https://api.github.com/users').subscribe((data) => {
+      this.userName = data.total;
+      console.log(this.userName);
+    });
+  }
+  //get profile info
+  getUserInfo() {
+    return this.http.get('https://api.github.com/users/' + this.userName); }
+  //get repo information
+  getUserRepos() {
+    return this.http.get(
+      'https://api.github.com/users/' + this.userName + '/repos'
+    );  }
+
+    getProfiles() {
+      return this.http.get<any[]>(this._Url);  }
+
+       getRepos(user: any){
+        let userrepo = 
+        this.http.get('https://api.github.com/users/'+this.userName+'/repos?acess_token='+environment.apiToken); 
+        console.log(userrepo)
+        return userrepo    
+      }
+      
+      getProfileInfo(){  
+        return this.http.get('https://api.github.com/users/'+this.userName+'?acess_token='+environment.apiToken);   
+       } 
+      updateProfile(userName:string){
+        this.userName = userName;
+      }
+
+      
+        
+        
+
+
+
+
+  // ngOnInit() {
+  //   // GET request with response type <any>
+  //   this.http.get<any>('https://api.github.com/users').subscribe((data) => {
+  //     this.userName = data.total;
+  //     console.log(this.userName);
+  //   });
+  // }
+//   getUser() {
+//     return this.http.get("https://api.github.com/users/" + this.userName).pipe(map(data => {
+//       return data;
+//     }));
+// }
 }
